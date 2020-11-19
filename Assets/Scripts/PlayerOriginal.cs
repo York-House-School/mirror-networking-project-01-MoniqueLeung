bb1e80729b0f856738eafccb09aab4290c10615c
Monique Leung <moniqueL22@yorkhouse.ca>
2020-11-18 22:30:24 -0800

    changed the player to another one because my ragdoll was malfunctioning unfortunately

---
+++ Assets/Scripts/PlayerOriginal.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class PlayerOriginal : NetworkBehaviour
+{
+    // Start is called before the first frame update
+    void HandleMovement()
+    {
+      if (isLocalPlayer){
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal*0.1f, moveVertical*0.1f,0);
+        transform.position = transform.position + movement;
+      }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      HandleMovement();
+    }
+}
