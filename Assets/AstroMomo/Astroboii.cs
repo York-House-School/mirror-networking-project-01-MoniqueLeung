8e947f707a5586eaa6ea611c7508aca9b1ca998c
momo leung <moniqueL22@yorkhouse.ca>
2021-01-12 15:55:29 -0800

    started teleportation concept

--- Assets/AstroMomo/Astroboii.cs
+++ Assets/AstroMomo/Astroboii.cs
@@ -48,7 +48,7 @@ namespace AstroMomo
     {
       controller = GetComponent <CharacterController>();
 
-      
+
     }
 
     void Update()
---
+++ Assets/AstroMomo/Teleportation/T1.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using Mirror;
+
+//Script by Monique Leung November-January2020 for Computer Programming 12 G Project Name = EXHILE
+namespace AstroMomo
+{
+  public class T1 : NetworkBehaviour
+  {
+      public Transform teleportTarget;
+      public GameObject astroboiiPlayer;
+      // Start is called before the first frame update
+      void Start()
+      {
+
+      }
+
+      // Update is called once per frame
+      void Update()
+      {
+        
+      }
+
+      void OnTriggerEnter(Collider other)
+      {
+        astroboiiPlayer.transform.position = teleportTarget.transform.position;
+      }
+
+  } //public class last bracket
+} //namespace bracket
