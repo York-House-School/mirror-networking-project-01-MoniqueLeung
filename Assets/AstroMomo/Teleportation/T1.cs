5ad7abb9a2d8cc4082d216c23463b0bf6f829d04
momo leung <moniqueL22@yorkhouse.ca>
2021-02-09 07:44:57 -0800

    finished teleportation feature of game currently in working version

--- Assets/AstroMomo/Teleportation/T1.cs
+++ Assets/AstroMomo/Teleportation/T1.cs
@@ -9,27 +9,22 @@ namespace AstroMomo
 {
   public class T1 : NetworkBehaviour
   {
-      //the public transform allows for the player to be teleported
-      public Transform teleportTarget;
-      public GameObject astroboiiPlayer;
-      // Start is called before the first frame update
-      //This void Start here is actually uneeded due to the fact that start doesn't do anything in this script.
+      
       void Start()
       {
 
       }
-      // Update is called once per frame
       void Update()
       {
 
       }
       //The reason why this is a OnTriggerEnter command here is because once the player comes in contact with
       //the collider of the teleportation platform than this void will run instantly.
-      void OnTriggerEnter(Collider other)
+      void OnTriggerEnter(Collider Col)
       {
-        astroboiiPlayer.transform.position = teleportTarget.transform.position;
+        Col.transform.position = new Vector3(Random.Range(-10.0f,10.0f), Col.transform.position.y, Random.Range (-10.0f, 10.0f));
       }
-      //this line transforms the players position from one teleportation platform to the next.
+      //this line transforms the players position to another random location
 
   } //public class last bracket
 } //namespace bracket
