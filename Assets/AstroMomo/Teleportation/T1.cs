a02f1cde29d73da0f8b5496c24db24aea5db7695
momo leung <moniqueL22@yorkhouse.ca>
2021-02-09 22:50:27 -0800

    made changes to menu and start scene

--- Assets/AstroMomo/Teleportation/T1.cs
+++ Assets/AstroMomo/Teleportation/T1.cs
@@ -22,7 +22,7 @@ namespace AstroMomo
       //the collider of the teleportation platform than this void will run instantly.
       void OnTriggerEnter(Collider Col)
       {
-        Col.transform.position = new Vector3(Random.Range(-10.0f,10.0f), Col.transform.position.y, Random.Range (-10.0f, 10.0f));
+        Col.transform.position = new Vector3((10.0f), Col.transform.position.y,(10.0f));
       }
       //this line transforms the players position to another random location
 
